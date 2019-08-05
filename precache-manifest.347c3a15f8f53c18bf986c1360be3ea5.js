88b96be8574a63040dfaae3cd27f2d6bd568ef2c
Svet <stsupryk@gmail.com>
2019-08-05 15:42:48 +0300

    Updates

---
+++ precache-manifest.347c3a15f8f53c18bf986c1360be3ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e385394c3e8e762d7873b9383ff17e0",
+    "url": "/Table_with_Redux/index.html"
+  },
+  {
+    "revision": "2bd0a97c9721cac3a83d",
+    "url": "/Table_with_Redux/static/css/main.be1eaf2d.chunk.css"
+  },
+  {
+    "revision": "8ae1fbad579af30d0c3b",
+    "url": "/Table_with_Redux/static/js/2.452bb36b.chunk.js"
+  },
+  {
+    "revision": "2bd0a97c9721cac3a83d",
+    "url": "/Table_with_Redux/static/js/main.a8d72a39.chunk.js"
+  },
+  {
+    "revision": "0e7e8e6597de49c95be4",
+    "url": "/Table_with_Redux/static/js/runtime~main.38ef8176.js"
+  }
+]);
